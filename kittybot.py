49e39332e0037254ce232eba15ebafca4b15c943
Alfir Kalimullin <alfir.kalimullin@yandex.ru>
2021-10-17 00:24:42 +0500

    Added baby photo show

--- kittybot.py
+++ kittybot.py
@@ -1,20 +1,20 @@
-# kittybot/kittybot.py
+import logging
 import os
 import sys
+from random import randint
+
 import requests
 from dotenv import load_dotenv
 from telegram import Bot, ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-import logging
 
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
+PIXABAY = os.getenv('PIXABAY')
 URL_cat = 'https://api.thecatapi.com/v1/images/search'
 URL_dog = 'https://api.thedogapi.com/v1/images/search'
 bot = Bot(token=TELEGRAM_TOKEN)
-# text = 'Вам телеграмма!'
-# bot.send_message(chat_id, text)
+
 logging.basicConfig(
     stream=sys.stdout,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -32,12 +32,19 @@ def get_new_image(animal):
             response = requests.get(URL_dog)
         except Exception as error:
             logging.error(f'Ошибка при запросе к основному API: {error}')
-
-
     response = response.json()
     random_animal = response[0].get('url')
     return random_animal
 
+def get_new_image_baby():
+    random_page = randint(1,15)
+    random_val = randint(1,19)
+    URL = f'https://pixabay.com/api/?key={PIXABAY}&q=новорожденный&image_type=photo&lang=ru&page={random_page}'
+    try:
+        response = requests.get(URL).json().get('hits')[random_val].get('webformatURL')
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+    return response
 
 def new_cat(update, context):
     chat = update.effective_chat
@@ -47,6 +54,9 @@ def new_dog(update, context):
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_image('dog'))
 
+def new_baby(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_image_baby())
 
 def say_hi(update, context):
     chat = update.effective_chat
@@ -59,6 +69,7 @@ def wake_up(update, context):
     button = ReplyKeyboardMarkup([
                 ['/new_cat'],
                 ['/new_dog'],
+                ['/new_baby'],
             ], resize_keyboard=True)
 
     context.bot.send_message(chat_id=chat.id,
@@ -73,6 +84,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('start', wake_up))
     updater.dispatcher.add_handler(CommandHandler('new_cat', new_cat))
     updater.dispatcher.add_handler(CommandHandler('new_dog', new_dog))
+    updater.dispatcher.add_handler(CommandHandler('new_baby', new_baby))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
     updater.start_polling(poll_interval=10.0)
     updater.idle()
